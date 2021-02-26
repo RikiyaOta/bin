d02edf36ac05345d22dbc9c740b6acd90f5232b4
rikiyaota <rikiya0421.fcb6@gmail.com>
2021-02-26 21:53:08 +0900

    First Commit.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+echo "Creating symbolic link now..."
+
+CURRENT_DIR=$(pwd)
+
+for FILE in $(ls .)
+do
+    [[ "$FILE" == ".git" ]] && continue
+    [[ "$FILE" == ".DS_Store" ]] && continue
+    [[ "$FILE" == "install.sh" ]] && continue
+
+    ln -sf $CURRENT_DIR/$f /usr/local/bin/$FILE
+done
+
+echo "Finished creating symbolic link!"
+
+exit 0

9d0a914409afa2febcffc942e0848ede12f58bc3
rikiyaota <rikiya0421.fcb6@gmail.com>
2022-07-05 13:06:37 +0900

    Fix the install script.

--- install.sh
+++ install.sh
@@ -8,12 +8,13 @@ CURRENT_DIR=$(pwd)
 
 for FILE in $(ls .)
 do
+    [[ "$FILE" == "README.md" ]] && continue
     [[ "$FILE" == ".git" ]] && continue
     [[ "$FILE" == ".DS_Store" ]] && continue
     [[ "$FILE" == "tags" ]] && continue
     [[ "$FILE" == "install.sh" ]] && continue
 
-    ln -sf "$CURRENT_DIR/$FILE" "/usr/local/bin/$FILE"
+    sudo ln -sf "$CURRENT_DIR/$FILE" "/usr/local/bin/$FILE"
 done
 
 echo "Finished creating symbolic link!"
